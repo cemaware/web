d36069baff5300f383e68210af8bb20f8a876ad8
fanchDev <51495972+eIfo1@users.noreply.github.com>
2023-01-08 19:18:08 -0500

    removed testing items from badges page

--- badges.php
+++ badges.php
@@ -4,12 +4,6 @@ include("$_SERVER[DOCUMENT_ROOT]/cema/header.php");
 
 ?>
 
-<?=$cema->determineUserLevel(5600)?>
-<br>
-<?=$cema->determinePercentageEXP(5600);?>
-<br>
-<?=$cema->determineXPtoLevelUp(101)?>
-
 <?php
 
 $cema->footer();
